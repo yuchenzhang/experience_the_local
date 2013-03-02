d9e06361237bd5eb86c5d91ac44c57643fc5b938
Yuchen Zhang <yuchenzhang@me.com>
2013-03-02 11:17:58 +0100

    added all rest of the starting kit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_traveller!
+  
+  private
+  def authenticate_user!
+    redirect_to root_url if current_traveller.nil?
+  end 
 end
