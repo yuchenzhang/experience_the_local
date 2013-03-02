2e57118d66099170913ac161403b164d98a74b85
Yuchen Zhang <yuchenzhang@me.com>
2013-03-02 10:24:41 +0100

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExperienceTheLocal::Application.config.secret_token = '22cdbb646d3a24efe041e6756b0e930d975d3fe8049828f08589e2db395a7d59420ccd10e18f9b9b4d61e111942ce3126fade459b00bb692087d36e94c1c4cb7'
